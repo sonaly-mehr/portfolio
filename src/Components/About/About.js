05b242a1891c7f3fcd73b96cb084826055f3dbe3
sonaly-mehr <sonaly.mehr@gmail.com>
2021-09-22 15:55:15 +0600

    resume-apdate

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -3,6 +3,7 @@ import './About.css'
 import women from '../../img/woman3.jpg'
 import AOS from 'aos';
 import 'aos/dist/aos.css'; // You can also use <link> for styles
+import resume from '../../../src/resume.pdf';
 // ..
 AOS.init();
 
@@ -17,7 +18,7 @@ const About = () => {
                         <div class="about-me" data-aos="fade-up" data-aos-easing="ease-out-cubic" data-aos-duration="2000">
                             <h4>I help you to make beauiful websites</h4>
                             <p>I am a junior full stack web developer. Currenty i am pursuing my Bsc in CSE at American International University - Bangladesh. Talking about my strengths &amp; skills, I am innovative, hardworking and quick learner. Seeking a responsible position to use my abilities and skill effectively and efficiently.</p>
-                            <a href="/static/media/Resume-converted.a96fdc7d.pdf" download="Resume-converted.pdf">Download Resume</a>
+                            <a href={resume} download="resume.pdf">Download Resume</a>
                         </div>
                     </div>
                     <div class="col-md-5">
